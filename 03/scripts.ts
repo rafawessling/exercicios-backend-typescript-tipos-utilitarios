8746ea7cc96ff460d67f8e2823c19b6d5c4469ef
rafawessling <rafaoening@gmail.com>
2024-02-09 23:54:03 +0100

    Added exercises utilitarian types

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -1,15 +1,20 @@
 type Conn = {
-    username: string,
-    password: string,
-    host: string,
-    port: string,
-    dbname: string
-}
+    username: string;
+    password: string;
+    host: string;
+    port: string;
+    dbname: string;
+};
 
-const conexao = (dados: Conn) => {
+type Conexao = {
+    driver: string;
+    url: string;
+};
+
+const conexao = (dados: Conn): Readonly<Conexao> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
-        url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
+        url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
     };
-}
+};
